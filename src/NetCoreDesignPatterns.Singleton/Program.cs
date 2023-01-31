9b7f3998f8ee2ad6941f9b81cb092997a8f12ad5
Bruno Paes <bruno.paes@adp.com>
2023-01-31 15:57:40 -0300

    add singleton

---
+++ src/NetCoreDesignPatterns.Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using NetCoreDesignPatterns.Singleton;
+
+var numberOfIterations = 25;
+
+Console.WriteLine("Singleton");
+
+Console.WriteLine("Parallel operations should not change singleton instance ID");
+Console.WriteLine($"Running for '{numberOfIterations}' items");
+
+
+ParallelOptions parallelOptions = new()
+{
+    MaxDegreeOfParallelism = numberOfIterations
+};
+
+Parallel.For(0, numberOfIterations, parallelOptions, index =>
+{
+    Console.WriteLine(Singleton.Instance.InstanceId);
+});
---
+++ src/NetCoreDesignPatterns.Singleton/Singleton.cs
@@ -0,0 +1,16 @@
+﻿namespace NetCoreDesignPatterns.Singleton
+{
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance { get { return lazy.Value; } }
+
+        public Guid InstanceId { get; set; }
+
+        private Singleton() 
+        {
+            InstanceId = Guid.NewGuid();
+        }
+    }
+}
