5f4f125063c461c831c6bd51a64c25b46b70a8cb
Bruno Paes <bruno.paes@adp.com>
2023-01-31 13:36:51 -0300

    add comments to simple factory

--- src/NetCoreDesignPatterns.SimpleFactory/UserFactory.cs
+++ src/NetCoreDesignPatterns.SimpleFactory/UserFactory.cs
@@ -2,6 +2,16 @@
 
 namespace NetCoreDesignPatterns.SimpleFactory
 {
+    // The Simple factory pattern  describes a class that has one creation method
+    // with a large conditional that based on method parameters chooses which
+    // product class to instantiate and then return.
+    // In most cases, a simple factory is an intermediate step of introducing
+    // Factory Method or Abstract Factory patterns.
+    // A simple factory is usually represented by a single method in a single
+    // class. Over time, this method might become too big, so you may decide
+    // to extract parts of the method to subclasses. Once you do it several
+    // times, you might discover that the whole thing turned into the
+    // classic factory method pattern.
     public class UserFactory
     {
         public static IUser CreateUser(string type)
