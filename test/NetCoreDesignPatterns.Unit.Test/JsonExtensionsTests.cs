70a52dd494d2c57782d97e4ded1a3781955b66bf
Bruno Paes <bruno.paes@adp.com>
2023-01-31 15:37:31 -0300

    update uni tests

---
+++ test/NetCoreDesignPatterns.Unit.Test/JsonExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using NetCoreDesignPatterns.Common.Extensions;
+using NetCoreDesignPatterns.SimpleFactory.Models;
+
+namespace NetCoreDesignPatterns.Unit.Test
+{
+    public class JsonExtensionsTests
+    {
+        [Fact]
+        public void ToJson_WhenValidAnnonymous_ExpectStringInJson()
+        {
+            // Assamble
+            var client = new
+            {
+                Id = Guid.NewGuid(),
+                Name = "name"
+            };
+
+            // Act
+            var jsonString = client.ToJson();
+
+            // Assert
+            Assert.NotNull(jsonString);
+        }
+
+        [Fact]
+        public void FromJson_WhenValidObject_ExpectDeserialization()
+        {
+            // Assamble
+            var jsonString = @"{""Id"":""6ee96f7a-76c1-4f9b-9f17-d8982d40a016"",""YearsAsDeveloper"":5}";
+
+            // Act
+            var developer = jsonString.FromJson<Developer>();
+
+            // Assert
+            Assert.NotNull(developer);
+        }
+    }
+}
